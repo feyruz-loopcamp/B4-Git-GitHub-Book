197f03af83ed26a7cceeb3e80cb6f19ec7976fd7
feyruz-loopcamp <feyruz@loopcamp.io>
2024-11-24 10:24:13 -0500

    added another print statement

--- src/chapter1/Page1.java
+++ src/chapter1/Page1.java
@@ -3,5 +3,6 @@ package chapter1;
 public class Page1 {
     public static void main(String[] args) {
         System.out.println("Feyruz added this line");
+        System.out.println("Feyruz added another line of code");
     }
 }
